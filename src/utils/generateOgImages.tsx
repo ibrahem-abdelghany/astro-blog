69c88aebcd1be673e0253ffa28be06f8b0152357
Ibrahem <ibrahem@abdelghany.com>
2025-08-01 20:09:01 +0300

    Update generateOgImages.tsx

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -7,13 +7,13 @@ import siteOgImage from "./og-templates/site";
 const fetchFonts = async () => {
   // Regular Font
   const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf"
+    "IBMPlexSansArabic-SemiBold.ttf"
   );
   const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
 
   // Bold Font
   const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf"
+    "IBMPlexSansArabic-SemiBold.ttf"
   );
   const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
 
