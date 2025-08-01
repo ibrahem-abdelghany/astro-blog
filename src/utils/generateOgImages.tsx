f4cedffa1ffd651e91b46d59fcb9efc8c9f647ba
Ibrahem <ibrahem@abdelghany.com>
2025-08-01 20:22:25 +0300

    .

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -7,13 +7,13 @@ import siteOgImage from "./og-templates/site";
 const fetchFonts = async () => {
   // Regular Font
   const fontFileRegular = await fetch(
-    "https://astro-blog-2udls3ghh-ibrahem-projects.vercel.app/IBMPlexSansArabic-SemiBold.ttf/IBMPlexSansArabic-SemiBold.ttf"
+    "https://astro-blog-tau-khaki.vercel.app/IBMPlexSansArabic-SemiBold.ttf"
   );
   const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
 
   // Bold Font
   const fontFileBold = await fetch(
-    "https://astro-blog-2udls3ghh-ibrahem-projects.vercel.app/IBMPlexSansArabic-SemiBold.ttf/IBMPlexSansArabic-SemiBold.ttf"
+    "https://astro-blog-tau-khaki.vercel.app/IBMPlexSansArabic-SemiBold.ttf"
   );
   const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
 
